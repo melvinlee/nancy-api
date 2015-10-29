44261bddd6dfbe9122492d69529d14f90883f4c2
Melvin Lee <melvinlee@hotmail.com>
2015-10-29 12:51:40 +0800

    Inject TunnelRepository as singleton for persistance data source

--- src/nancy-api/Bootstrapper.cs
+++ src/nancy-api/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Nancy;
+using NancyApi.Repository;
 
 namespace NancyApi
 {
@@ -7,5 +8,13 @@ namespace NancyApi
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            // Register repository as singleton for persistant data source
+            container.Register<TunnelRepository>().AsSingleton();
+        }
     }
 }
