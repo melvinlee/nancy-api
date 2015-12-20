9b081f6eb3d4f826f5bf3a689add6485a5ccf8ed
Melvin Lee <melvinlee@hotmail.com>
2015-12-20 22:39:30 +0800

    feat: add Negotiate Extension

---
+++ src/nancy-api/NegotiateExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Nancy;
+using Nancy.Responses.Negotiation;
+
+namespace NancyApi
+{
+    public static class NegotiateExtensions
+    {
+        public static Negotiator AsGetResponse(this Negotiator negotiator, object model)
+        {
+            return negotiator.AsXmlJsonReponse(model);
+        }
+
+        public static Negotiator AsPostReponse(this Negotiator negotiator, object model)
+        {
+            return negotiator.AsXmlJsonReponse(model, HttpStatusCode.Accepted);
+        }
+
+        public static Negotiator AsXmlJsonReponse(this Negotiator negotiator, object model, HttpStatusCode httpStatusCode = HttpStatusCode.OK)
+        {
+            if (model == null)
+            {
+                httpStatusCode = HttpStatusCode.NoContent;
+            }
+
+            return negotiator.WithModel(model)
+                .WithAllowedMediaRange(new MediaRange("application/xml"))
+                .WithAllowedMediaRange(new MediaRange("application/json"))
+                .WithStatusCode(httpStatusCode);
+        }
+    }
+}
